b3d7bb569ef484b50ef098b09655b1ddbe12ab4c
moehr1z <mooehr1z@gmail.com>
2025-07-02 15:35:03 +0200

    Bugfixes for client server view

--- src/org/eclipse/tracecompass/l4trace/client_server/ClientServerView.java
+++ src/org/eclipse/tracecompass/l4trace/client_server/ClientServerView.java
@@ -1,230 +1,195 @@
+// ClientServerView.java
 package org.eclipse.tracecompass.l4trace.client_server;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
-import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
-import org.eclipse.tracecompass.tmf.core.event.TmfEvent;
-import org.eclipse.tracecompass.tmf.core.request.ITmfEventRequest;
-import org.eclipse.tracecompass.tmf.core.request.TmfEventRequest;
-import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
-import org.eclipse.tracecompass.tmf.core.signal.TmfTraceSelectedSignal;
-import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimeRange;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.tracecompass.tmf.core.event.*;
+import org.eclipse.tracecompass.tmf.core.request.*;
+import org.eclipse.tracecompass.tmf.core.signal.*;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.ui.views.TmfView;
-import org.eclipse.ui.part.ViewPart;
-
 
 public class ClientServerView extends TmfView {
-	private static final String VIEW_ID = "org.eclipse.tracecompass.l4trace.client_server.view";
-	private TableViewer viewer;
+    private static final String VIEW_ID = "org.eclipse.tracecompass.l4trace.client_server.view";
+    private TableViewer viewer;
     private ITmfTrace currentTrace;
-    private ThreadProvider threads; 
-    private Long lastEventNumber = 0l;
+    private final ThreadProvider threads = new ThreadProvider();
+    private Long lastEventNumber = 0L;
 
     public ClientServerView() {
         super(VIEW_ID);
-		threads = new ThreadProvider();
     }
 
     @Override
     public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
-		
-		Table table = viewer.getTable();
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		
-		viewer.setContentProvider(ArrayContentProvider.getInstance());
-		
-		TableViewerColumn colThreadId = new TableViewerColumn(viewer, SWT.NONE);
-		colThreadId.getColumn().setWidth(200);
-		colThreadId.getColumn().setText("Thread ID");
-		colThreadId.setLabelProvider(new ColumnLabelProvider() {
-		    @Override
-		    public String getText(Object element) {
-		    	Thread t = (Thread) element;
-		    	System.out.println(t.getId());
-		    	return t.getId();
-		    }
-		});
+        viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+                | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+        Table table = viewer.getTable();
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
+        viewer.setContentProvider(ArrayContentProvider.getInstance());
 
-		TableViewerColumn colTotal = new TableViewerColumn(viewer, SWT.NONE);
-		colTotal.getColumn().setWidth(200);
-		colTotal.getColumn().setText("Total Time");
-		colTotal.setLabelProvider(new ColumnLabelProvider() {
-		    @Override
-		    public String getText(Object element) {
-		    	Thread t = (Thread) element;
-		    	return t.getTotalTime();
-		    }
-		});
-
-		TableViewerColumn colSelf = new TableViewerColumn(viewer, SWT.NONE);
-		colSelf.getColumn().setWidth(200);
-		colSelf.getColumn().setText("Self Time");
-		colSelf.setLabelProvider(new ColumnLabelProvider() {
-		    @Override
-		    public String getText(Object element) {
-		    	Thread t = (Thread) element;
-		    	return t.getSelfTime();
-		    }
-		});
-
-		TableViewerColumn colImposed = new TableViewerColumn(viewer, SWT.NONE);
-		colImposed.getColumn().setWidth(200);
-		colImposed.getColumn().setText("Client Time");
-		colImposed.setLabelProvider(new ColumnLabelProvider() {
-		    @Override
-		    public String getText(Object element) {
-		    	Thread t = (Thread) element;
-		    	return t.getImposedTime();
-		    }
-		});
-
-		TableViewerColumn colImposedDetails = new TableViewerColumn(viewer, SWT.NONE);
-		colImposedDetails.getColumn().setWidth(500);
-		colImposedDetails.getColumn().setText("Client Time Details");
-		colImposedDetails.setLabelProvider(new ColumnLabelProvider() {
-		    @Override
-		    public String getText(Object element) {
-		    	Thread t = (Thread) element;
-		    	return t.getImposedTimeDetails();
-		    }
-		});
-		
+        String[] titles = { "Thread ID", "Thread Name", "Total Time", "Self Time", "Client Time", "Client Time Details" };
+        int[] widths   = { 50,         100, 			100,          100,         100,           300 };
+        for (int i = 0; i < titles.length; i++) {
+            final int index = i;  // must be effectively final for the label provider
+            TableViewerColumn col = new TableViewerColumn(viewer, SWT.NONE);
+            col.getColumn().setText(titles[index]);
+            col.getColumn().setWidth(widths[index]);
+            col.setLabelProvider(new ColumnLabelProvider() {
+                @Override
+                public String getText(Object element) {
+                    Thread t = (Thread) element;
+                    switch (titles[index]) {
+                        case "Thread ID":             return t.getId();
+                        case "Thread Name":           return t.getName();
+                        case "Total Time":            return t.getTotalTime();
+                        case "Self Time":             return t.getSelfTime();
+                        case "Client Time":           return t.getImposedTime();
+                        case "Client Time Details":   return t.getImposedTimeDetails();
+                        default:                      return "";
+                    }
+                }
+            });
+        }
     }
 
     @Override
     public void setFocus() {
-    	viewer.getControl().setFocus();
-
+        viewer.getControl().setFocus();
     }
-    
+
+    /** Dump all key fields of a thread to stderr with a custom tag. */
+    private void dumpThread(Thread t, String tag) {
+        long total = Long.parseLong(t.getTotalTime());
+        long imposed = Long.parseLong(t.getImposedTime());
+        System.err.printf(
+            "[%s] Thread %s → total=%d, imposedSum=%d, start=%d, potImposed=%d, details=%s%n",
+            tag, t.getId(), total, imposed,
+            t.getPotentialImposedStart(), t.getPotentialImposedTime(),
+            t.getImposedTimeDetails()
+        );
+    }
+
     @TmfSignalHandler
-    public void traceSelected(final TmfTraceSelectedSignal signal) {
-        // Don't populate the view again if we're already showing this trace
+    public void traceSelected(TmfTraceSelectedSignal signal) {
         if (currentTrace == signal.getTrace()) {
             return;
         }
         currentTrace = signal.getTrace();
 
-
-        // Create the request to get data from the trace
-        TmfEventRequest req = new TmfEventRequest(TmfEvent.class, 0, ITmfEventRequest.ALL_DATA,
-                ITmfEventRequest.ExecutionType.BACKGROUND) {
+        TmfEventRequest req = new TmfEventRequest(TmfEvent.class, 0,
+                ITmfEventRequest.ALL_DATA, ITmfEventRequest.ExecutionType.BACKGROUND) {
 
             @Override
             public void handleData(ITmfEvent event) {
-                // Called for each event
                 super.handleData(event);
+
+                // 1) Handle dropped events
+                long evNum = Long.parseLong(event.getContent().getFieldValue(String.class, "context._event_count"));
+                // TODO remove
+//                if (evNum > 300) {
+//                	return;
+//                }
                 
-                Long eventNumber = Long.parseLong(event.getContent().getFieldValue(String.class, "context._event_count")); //$NON-NLS-1$
-                // Events were dropped
-                if (eventNumber - lastEventNumber > 1) {		
-                	System.out.println("Dropped events." + eventNumber + "\t" + lastEventNumber);
-                	for (Thread thread: threads.getThreads()) {
-                		thread.clearPotentialImposedTime();
-                		thread.setPotentialImposed(false);
-                	}
-                	lastEventNumber = eventNumber;
-                	return;
+                if (evNum - lastEventNumber > 1) {
+                    System.err.println("[RESET] Dropped events: " + (evNum - lastEventNumber));
+                    for (Thread t : threads.getThreads()) {
+                        t.clearPotentialImposedTime();
+                        t.setPotentialImposed(false);
+                    }
+                    lastEventNumber = evNum;
+                    return;
                 }
-                
-				lastEventNumber = eventNumber;
-                
-				String threadId = event.getContent().getFieldValue(String.class, "context._dbg_id"); //$NON-NLS-1$
-				Thread thread = threads.get(threadId);
-				if (thread == null) {
-					thread = new Thread(threadId);
-					threads.put(threadId, thread);
-				} 
-				Long timestamp = event.getTimestamp().getValue();
-                
-                if (event.getName().equals("IPC")) {
-					String operation = event.getContent().getFieldValue(String.class, "type_");
+                lastEventNumber = evNum;
 
-					// Thread replies, we assume it to be some server
-					if (operation.equals("Reply") || operation.equals("ReplyAndWait")) {
-						String clientThread = event.getContent().getFieldValue(String.class, "dbg_id"); //$NON-NLS-1$
-						thread.addPotentialImposedTime(timestamp - thread.getPotentialImposedStart());
-						thread.commitPotentialImposed(clientThread);	
-					}
-					
-					if (operation.equals("Send") || operation.equals("Call")) { 
-						thread.setPotentialImposed(false);
-						thread.clearPotentialImposedTime();
-					}
-                } else if (event.getName().equals("IpcRes")) { 
-					String operation = event.getContent().getFieldValue(String.class, "type_");
+                // 2) Identify thread & timestamp
+                String tid = event.getContent().getFieldValue(String.class, "context._dbg_id");
+                String name = event.getContent().getFieldValue(String.class, "context._name");
+                Thread th = threads.get(tid);
+                if (th == null) {
+                    th = new Thread(tid);
+                    threads.put(tid, th);
+                    System.err.println("[NEW THREAD] " + tid);
+                }
+                th.setName(name);
+                long ts = event.getTimestamp().getValue();
 
-					// Thread waited for some IPC, so possibly a server
-					if (operation.equals("Recv") || operation.equals("OpenWait") || operation.equals("Wait") || operation.equals("ReplyAndWait")) {
-						thread.setPotentialImposed(true);
-						thread.setPotentialImposedStart(timestamp);
-					} 
-                } else if (event.getName().equals("sched_switch")) {
-                	// handle scheduled out thread
-                	String prevThreadId = event.getContent().getFieldValue(String.class, "prev_tid"); //$NON-NLS-1$
-                	Thread prevThread = threads.get(prevThreadId);
-                	if (prevThread == null) {
-						prevThread = new Thread(prevThreadId);
-						threads.put(prevThreadId, prevThread);
+                // 3) Dispatch by event type
+                switch (event.getName()) {
+                case "IPC": {
+                	if (th.getIsScheduled()) {
+						String op = event.getContent().getFieldValue(String.class, "type_");
+						if (op.equals("Reply") || op.equals("ReplyAndWait")) {
+							if (th.getPotentialImposed()) {
+								String client = event.getContent().getFieldValue(String.class, "dbg_id");
+								long delta = ts - th.getPotentialImposedStart();
+								th.addPotentialImposedTime(delta);
+								th.commitPotentialImposed(client);
+							}
+						} else if (op.equals("Send") || op.equals("Call") || op.equals("CallIpc")) {
+							th.setPotentialImposed(false);
+							th.clearPotentialImposedTime();
+						}
                 	}
-                	Long lastSchedTime = prevThread.getLastSchedTime();
-                	if (lastSchedTime != 0) {
-						prevThread.addTotalTime(timestamp - lastSchedTime);
-                	}
-                	if (prevThread.getPotentialImposed()) {
-                		prevThread.addPotentialImposedTime(timestamp - prevThread.getPotentialImposedStart()); 
-                	}
-
-                	// handle scheduled in thread
-                	String nextThreadId = event.getContent().getFieldValue(String.class, "next_tid"); //$NON-NLS-1$
-                	Thread nextThread = threads.get(nextThreadId);
-                	if (nextThread == null) {
-						nextThread = new Thread(nextThreadId);
-						threads.put(nextThreadId, nextThread);
-                	}
-                	nextThread.setLastSchedTime(timestamp);
-                	if (nextThread.getPotentialImposed()) {
-                		nextThread.setPotentialImposedStart(timestamp);
+                    break;
+                }
+                case "IPCRES": {
+                	if (th.getIsScheduled()) {
+						String op2 = event.getContent().getFieldValue(String.class, "type_");
+						if (op2.equals("Recv") || op2.equals("OpenWait")
+								|| op2.equals("Wait") || op2.equals("ReplyAndWait")) {
+							th.setPotentialImposed(true);
+							th.setPotentialImposedStart(ts);
+							th.clearPotentialImposedTime();
+						}
+						break;
                 	}
                 }
-                
+                case "sched_switch": {
+                    // scheduled-out
+                    String prevId = event.getContent().getFieldValue(String.class, "prev_tid");
+                    Thread prev = threads.get(prevId);
+                    if (prev == null) {
+                        prev = new Thread(prevId);
+                        threads.put(prevId, prev);
+                        System.err.println("[NEW prevThread] " + prevId);
+                    }
+                    prev.setIsScheduled(false);
+                    long lastTs = prev.getLastSchedTime();
+                    if (lastTs != 0) {
+                        prev.addTotalTime(ts - lastTs);
+                    }
+                    if (prev.getPotentialImposed()) {
+                        prev.addPotentialImposedTime(ts - prev.getPotentialImposedStart());
+                    }
+
+                    // scheduled-in
+                    String nextId = event.getContent().getFieldValue(String.class, "next_tid");
+                    Thread next = threads.get(nextId);
+                    if (next == null) {
+                        next = new Thread(nextId);
+                        threads.put(nextId, next);
+                        System.err.println("[NEW nextThread] " + nextId);
+                    }
+                    next.setIsScheduled(true);
+                    next.setLastSchedTime(ts);
+                    if (next.getPotentialImposed()) {
+                        next.setPotentialImposedStart(ts);
+                    }
+                    break;
+                }
+                } // switch
             }
 
             @Override
             public void handleSuccess() {
-                // Request successful, not more data available
                 super.handleSuccess();
-
-                // This part needs to run on the UI thread since it updates the chart SWT control
-                Display.getDefault().asyncExec(new Runnable() {
-
-                    @Override
-                    public void run() {
-                    }
-
-                });
+                Display.getDefault().asyncExec(() -> viewer.setInput(threads.getThreads()));
             }
         };
-        ITmfTrace trace = signal.getTrace();
-        trace.sendRequest(req);
-        
-		viewer.setInput(threads.getThreads());
+
+        signal.getTrace().sendRequest(req);
     }
-
-
 }
--- src/org/eclipse/tracecompass/l4trace/client_server/Thread.java
+++ src/org/eclipse/tracecompass/l4trace/client_server/Thread.java
@@ -4,117 +4,125 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Thread {
-	private String id;
-	private Long totalTime;
-	private Long imposedTime;
-	private Boolean potentialImposed;
-	private Long potentialImposedTime;
-	private Map<String, Long> imposedTimeDetails;
-	private Long lastSchedTime;
-	private Long potentialImposedStart;
-	
+    private final String id;
+    private Long totalTime = 0L;
+    private Boolean potentialImposed = false;
+    private Long potentialImposedTime = 0L;
+    private final Map<String, Long> imposedTimeDetails = new HashMap<>();
+    private Long lastSchedTime = 0L;
+    private Long potentialImposedStart = 0L;
+    private Boolean isScheduled = false;
+    private String name = "";
+
 
 	public Thread(String id) {
-		super();
-		this.potentialImposed = false;
-		this.potentialImposedTime = 0l;
-		this.id = id;
-		this.totalTime = 0l;
-		this.imposedTime = 0l;
-		this.imposedTimeDetails = new HashMap<String, Long>();
-		this.lastSchedTime = 0l;
-		this.potentialImposedStart = 0l;
-	}
-	
-	public Long getPotentialImposedStart() {
-		return potentialImposedStart;
-	}
+        this.id = id;
+        this.name = "";
+        debug("NEW", "");
+    }
 
-	public void setPotentialImposedStart(Long potentialImposedStart) {
-		this.potentialImposedStart = potentialImposedStart;
-	}
 
-	public Long getLastSchedTime() {
-		return lastSchedTime;
-	}
+    private boolean isDebug() {
+        return "12".equals(id);
+    }
 
-	public void setLastSchedTime(Long lastSchedTime) {
-		this.lastSchedTime = lastSchedTime;
-	}
+    private void debug(String field, String msg) {
+        if (isDebug()) {
+            System.err.printf("[Thread %s] %s → %s%n", id, field, msg);
+        }
+    }
 
-	public Boolean getPotentialImposed() {
-		return potentialImposed;
-	}
+    public Boolean getIsScheduled() { return isScheduled; }
+	public void setIsScheduled(Boolean isScheduled) { this.isScheduled = isScheduled; }
 
-	public void setPotentialImposed(Boolean potentialImposed) {
-		this.potentialImposed = potentialImposed;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void addPotentialImposedTime(Long time) {
-		this.potentialImposedTime += time;
-	}
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 
-	public void clearPotentialImposedTime() {
-		this.potentialImposedTime = 0l;
-	}
-	
-	public void commitPotentialImposed(String clientThread) {
-		Long clientTime = this.imposedTimeDetails.get(clientThread);
-		
-		if (clientTime == null) {
-			this.imposedTimeDetails.put(clientThread, this.potentialImposedTime);
-		} else {
-			this.imposedTimeDetails.replace(clientThread, clientTime + this.potentialImposedTime);
-		}
-		this.potentialImposedTime = 0l;
-	}
+    public String getTotalTime() {
+        return totalTime.toString();
+    }
 
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getTotalTime() {
-		// TODO convert
-		return totalTime.toString();
-	}
-	public void addTotalTime(Long totalTime) {
-		this.totalTime += totalTime;
-	}
-	public String getImposedTime() {
-		Long sum = 0l;
-		// TODO do this in commit imposed time and only return here
-		for (Long v: imposedTimeDetails.values()) {
-			sum += v;
-		}
-		
-		// TODO convert
+    public String getImposedTime() {
+        long sum = 0L;
+        for (long v : imposedTimeDetails.values()) sum += v;
+        return Long.toString(sum);
+    }
 
-		return sum.toString();
-	}
-	public void addImposedTime(String imposerId, Long imposedTime) {
-		Long i = this.imposedTimeDetails.get(imposerId);
-		if (i == null) {
-			this.imposedTimeDetails.put(imposerId, imposedTime);
-		} else {
-			i += imposedTime;
-		}
-		
-		this.imposedTime += imposedTime;
-	}
-	public String getImposedTimeDetails() {
-		// TODO convert
-	    StringBuilder mapAsString = new StringBuilder("{");
-	    for (String key : this.imposedTimeDetails.keySet()) {
-	        mapAsString.append(key + "=" + this.imposedTimeDetails.get(key).toString() + ", ");
-	    }
-	    mapAsString.delete(mapAsString.length()-2, mapAsString.length()).append("}");
-	    return mapAsString.toString();
-	}
-	public String getSelfTime() {
-		// TODO convert
-		Long selfTime = this.totalTime - this.imposedTime;
-		return selfTime.toString();
-	}
+    public String getImposedTimeDetails() {
+        if (imposedTimeDetails.isEmpty()) return "{}";
+        StringBuilder sb = new StringBuilder("{");
+        imposedTimeDetails.forEach((k,v) -> sb.append(k).append("=").append(v).append(", "));
+        sb.setLength(sb.length() - 2);
+        return sb.append("}").toString();
+    }
+
+    public String getSelfTime() {
+        long self = totalTime - Long.parseLong(getImposedTime());
+        return Long.toString(self);
+    }
+
+    public Long getLastSchedTime() {
+        return lastSchedTime;
+    }
+    public void setLastSchedTime(Long t) {
+//        debug("setLastSchedTime", "old=" + lastSchedTime + " new=" + t);
+        lastSchedTime = t;
+    }
+
+    public Boolean getPotentialImposed() {
+        return potentialImposed;
+    }
+    public void setPotentialImposed(Boolean b) {
+        debug("setPotentialImposed", "old=" + potentialImposed + " new=" + b);
+        potentialImposed = b;
+    }
+
+    public Long getPotentialImposedStart() {
+        return potentialImposedStart;
+    }
+    public void setPotentialImposedStart(Long t) {
+        debug("setPotentialImposedStart", "old=" + potentialImposedStart + " new=" + t);
+        potentialImposedStart = t;
+    }
+
+    public void clearPotentialImposedTime() {
+        debug("clearPotentialImposedTime", "old=" + potentialImposedTime + " new=0");
+        potentialImposedTime = 0L;
+    }
+
+    public void addPotentialImposedTime(Long t) {
+        debug("addPotentialImposedTime", "delta=" + t + " → " + potentialImposedTime + "+" + t);
+        potentialImposedTime += t;
+    }
+
+    public Long getPotentialImposedTime() {
+        return potentialImposedTime;
+    }
+
+    public void addTotalTime(Long delta) {
+//        debug("addTotalTime", "delta=" + delta + " → " + totalTime + "+" + delta);
+        totalTime += delta;
+    }
+
+    public void commitPotentialImposed(String clientThread) {
+        Long prev = imposedTimeDetails.get(clientThread);
+        long sum = (prev == null ? 0L : prev) + potentialImposedTime;
+        debug("commitPotentialImposed",
+              "client=" + clientThread +
+              " prev=" + prev +
+              " added=" + potentialImposedTime +
+              " → new=" + sum);
+        imposedTimeDetails.put(clientThread, sum);
+        potentialImposedTime = 0L;
+        potentialImposed = false;
+    }
 }
+
